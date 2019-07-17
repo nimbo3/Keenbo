357fb3bb6c27318be390c0effd0a404dc4dd411f
MohammadReza Pakzadian <pakzadianmrp@gmail.com>
2019-07-17 15:03:24 +0430

    complete ParserService class

--- src/main/java/in/nimbo/service/ParserService.java
+++ src/main/java/in/nimbo/service/ParserService.java
@@ -1,16 +1,34 @@
 package in.nimbo.service;
 
 import in.nimbo.entity.Page;
+import in.nimbo.exception.ParseLinkException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParserService {
-    public Page parse(String link){
+    private static final int TIMEOUT = 30000;
 
-//        Document parse = Jsoup.parse(url, config.getTimeout());
-//        Elements elements = parse.getElementsByTag("a");
-//        for (Element element : elements) {
-//
-//        }
-        // TODO process parsed object
-        return null;
+    public Page parse(String siteLink) {
+        List<String> links = new ArrayList<>();
+        try {
+            Document document = Jsoup.parse(new URL(siteLink), TIMEOUT);
+            Elements elements = document.getElementsByTag("a");
+            for (Element element : elements) {
+                links.add(element.absUrl("href"));
+            }
+            return new Page(document.html(), links);
+        } catch (MalformedURLException e) {
+            throw new ParseLinkException("unable to parse url: " + siteLink, e);
+        } catch (IOException e) {
+            throw new ParseLinkException("unable to parse url with jsoup", e);
+        }
     }
 }
