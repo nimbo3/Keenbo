9cd4d1b90bb9459bafaed0ee1005412fb9c1b0f9
DanialErfanian <erfaniiyanerfan@gmail.com>
2019-07-21 16:43:37 +0430

    Add ElasticCLI

---
+++ src/main/java/in/nimbo/dao/elastic/ElasticCLI.java
@@ -0,0 +1,35 @@
+package in.nimbo.dao.elastic;
+
+import in.nimbo.config.ElasticConfig;
+
+import java.util.Scanner;
+
+public class ElasticCLI {
+    public static void main(String[] args) {
+        ElasticDAO elasticDAO = new ElasticDAOImpl(ElasticConfig.load());
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String type = scanner.next();
+            switch (type) {
+                case "getAllLinks":
+                    System.out.println(elasticDAO.getAllLinks());
+                    break;
+                case "get": {
+                    String link = scanner.next();
+                    System.out.println(elasticDAO.get(link));
+                    break;
+                }
+                case "save": {
+                    String link = scanner.next(), text = scanner.next();
+                    elasticDAO.save(link, text);
+                    break;
+                }
+                case "exit":
+                    System.exit(0);
+                default:
+                    System.out.println("invalid input");
+                    break;
+            }
+        }
+    }
+}
