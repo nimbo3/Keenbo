04d4b98d5776d055e53693c052680c813bdb1822
MohammadReza Pakzadian <pakzadianmrp@gmail.com>
2019-07-23 17:49:30 +0430

    write parse test method

--- src/main/java/in/nimbo/service/ParserService.java
+++ src/main/java/in/nimbo/service/ParserService.java
@@ -17,6 +17,8 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.print.Doc;
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
@@ -35,17 +37,10 @@ public class ParserService {
     public Optional<Page> parse(String siteLink) {
         List<String> links = new ArrayList<>();
         try {
-            Connection.Response response = Jsoup.connect(siteLink)
-                    .userAgent(appConfig.getJsoupUserAgent())
-                    .timeout(appConfig.getJsoupTimeout())
-                    .followRedirects(true)
-                    .ignoreContentType(true)
-                    .execute();
-            if (response.contentType() != null &&
-                    !response.contentType().contains("text/html")) {
+            Optional<Document> documentOptional = getDocument(siteLink);
+            if (!documentOptional.isPresent())
                 return Optional.empty();
-            }
-            Document document = response.parse();
+            Document document = documentOptional.get();
             String pageContent = document.text();
             if (isEnglishLanguage(pageContent)) {
                 Elements elements = document.getElementsByTag("a");
@@ -58,16 +53,34 @@ public class ParserService {
                 }
                 return Optional.of(new Page(pageContent, links));
             }
+        } catch (LanguageDetectException e) {
+            logger.warn("cannot detect language of site : {}", siteLink);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Document> getDocument(String siteLink) {
+        try {
+            Connection.Response response = Jsoup.connect(siteLink)
+                    .userAgent(appConfig.getJsoupUserAgent())
+                    .timeout(appConfig.getJsoupTimeout())
+                    .followRedirects(true)
+                    .ignoreContentType(true)
+                    .execute();
+            if (response.contentType() != null &&
+                    !response.contentType().contains("text/html")) {
+                return Optional.empty();
+            } else {
+                return Optional.of(response.parse());
+            }
         } catch (MalformedURLException e) {
             logger.warn("Illegal url format: {}", siteLink);
         } catch (HttpStatusException e) {
             logger.warn("Response is not OK. Url: \"{}\" StatusCode: {}", e.getUrl(), e.getStatusCode());
-        }catch (LanguageDetectException e){
-            logger.warn("cannot detect language of site : {}", siteLink);
         } catch (IOException e) {
             logger.warn("Unable to parse page with jsoup: {}", siteLink);
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
         }
         return Optional.empty();
     }
--- src/test/java/in/nimbo/service/ParserServiceTest.java
+++ src/test/java/in/nimbo/service/ParserServiceTest.java
@@ -1,4 +1,51 @@
 package in.nimbo.service;
 
+import in.nimbo.TestUtility;
+import in.nimbo.config.AppConfig;
+import in.nimbo.entity.Page;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
 public class ParserServiceTest {
+    private static String input;
+    private static String persianInput;
+    private static Document document;
+    private static Document persianDocument;
+    private static ParserService parserService;
+    private static final String FILE_ADDRESS = "src/test/resources/html/sampleHTML.html";
+    private static final String PERSIAN_FILE_ADDRESS = "src/test/resources/html/sampleHTMLper.html";
+    private static String link = "https://google.com";
+    private static String pageContent = "Nimbo Link Header mail at support@nimbo.in. paragraph! another link";
+    private static List<String> pageLinks = new ArrayList<>();
+
+    @BeforeClass
+    public static void init() {
+        AppConfig appConfig = AppConfig.load();
+        input = TestUtility.getFileContent(Paths.get(FILE_ADDRESS));
+        persianInput = TestUtility.getFileContent(Paths.get(PERSIAN_FILE_ADDRESS));
+        document = Jsoup.parse(input, "UTF-8");
+        persianDocument = Jsoup.parse(persianInput, "UTF-8");
+        parserService = spy(new ParserService(appConfig));
+        pageLinks.add("http://nimbo.in");
+        pageLinks.add("https://google.com");
+    }
+
+    @Test
+    public void parseTest() {
+        doReturn(Optional.of(document)).when(parserService).getDocument(link);
+        Page page = parserService.parse(link).get();
+        Assert.assertEquals(page.getContent(), pageContent);
+        Assert.assertEquals(page.getLinks(), pageLinks);
+    }
 }
