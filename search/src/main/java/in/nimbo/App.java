76e30f8b486a58e911f55bab6302758274cb4be5
ehsan karimi <karimiehsan901@gmail.com>
2019-07-25 15:39:36 +0430

    Finish backend for search

--- search/src/main/java/in/nimbo/App.java
+++ search/src/main/java/in/nimbo/App.java
@@ -3,6 +3,7 @@ package in.nimbo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import in.nimbo.config.ElasticConfig;
+import in.nimbo.config.SparkConfig;
 import in.nimbo.controller.SearchController;
 import in.nimbo.dao.elastic.ElasticDAO;
 import in.nimbo.dao.elastic.ElasticDAOImpl;
@@ -14,17 +15,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Spark;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class App {
     private static Logger logger = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         ObjectMapper mapper = new ObjectMapper();
         ObjectWriter writer = mapper.writer();
         JsonTransformer transformer = new JsonTransformer(mapper, writer);
+
         ElasticConfig elasticConfig = ElasticConfig.load();
+        SparkConfig sparkConfig = SparkConfig.load();
+
         RestHighLevelClient restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(elasticConfig.getHost(), elasticConfig.getPort())));
         ElasticDAO elasticDAO = new ElasticDAOImpl(restHighLevelClient, elasticConfig);
         SearchController searchController = new SearchController(elasticDAO);
-//        Spark.port(8080);
+
+        Spark.port(sparkConfig.getPort());
         Spark.path("/", () -> {
             Spark.before("/*", (request, response) -> {
                 logger.info("new request for uri: " + request.uri());
@@ -33,14 +42,24 @@ public class App {
                 String query = request.queryParams("query");
                 return searchController.search(query != null ? query : "");
             }) , transformer);
-            Spark.get("/exception", ((request, response) -> {
-                throw new Exception("exception");
-            }));
-            Spark.exception(Exception.class, ((e, request, response) -> {
-                logger.error(e.getMessage(), e);
-                return;
-            }));
             Spark.after("/*", (request, response) -> logger.info("response sent successfully: " + request.uri()));
         });
+
+        outer: while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] split = line.split(" ");
+            if (split.length > 0){
+                switch (split[0]) {
+                    case "exit":
+                        try {
+                            restHighLevelClient.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Spark.stop();
+                        break outer;
+                }
+            }
+        }
     }
 }
---
+++ search/src/main/java/in/nimbo/config/SparkConfig.java
@@ -0,0 +1,32 @@
+package in.nimbo.config;
+
+import in.nimbo.exception.LoadConfigurationException;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+public class SparkConfig {
+    private static final String CONFIG_NAME = "spark.properties";
+    private int port;
+
+    public SparkConfig() {
+    }
+
+    public static SparkConfig load() {
+        SparkConfig sparkConfig = new SparkConfig();
+        try {
+            PropertiesConfiguration config = new PropertiesConfiguration(CONFIG_NAME);
+            sparkConfig.setPort(config.getInt("spark.port"));
+            return sparkConfig;
+        } catch (ConfigurationException e) {
+            throw new LoadConfigurationException(CONFIG_NAME, e);
+        }
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getPort() {
+        return port;
+    }
+}
