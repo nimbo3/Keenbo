c3e1c143a818206271cbdb9e972dfa64b571e38e
MohammadReza Pakzadian <pakzadianmrp@gmail.com>
2019-07-27 20:01:53 +0430

    implement syncWithElastic method in HBaseDAOImpl class

--- crawler/src/main/java/in/nimbo/dao/hbase/HBaseDAOImpl.java
+++ crawler/src/main/java/in/nimbo/dao/hbase/HBaseDAOImpl.java
@@ -1,15 +1,24 @@
 package in.nimbo.dao.hbase;
 
 import in.nimbo.config.HBaseConfig;
+import in.nimbo.dao.elastic.ElasticDAO;
 import in.nimbo.entity.Anchor;
 import in.nimbo.entity.Meta;
 import in.nimbo.entity.Page;
 import in.nimbo.exception.HBaseException;
+import in.nimbo.service.ParserService;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.BinaryComparator;
+import org.apache.hadoop.hbase.filter.CompareFilter;
+import org.apache.hadoop.hbase.filter.FamilyFilter;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
 
 public class HBaseDAOImpl implements HBaseDAO {
     private HBaseConfig config;
@@ -58,6 +67,31 @@ public class HBaseDAOImpl implements HBaseDAO {
         } catch (IllegalArgumentException e) {
             // It will be thrown if size of page will be more than hbase.client.keyvalue.maxsize = 10485760
             return false;
+        } catch (IOException e) {
+            throw new HBaseException(e);
+        }
+    }
+
+    @Override
+    public void syncWithElastic(ElasticDAO elasticDAO, ParserService parserService) {
+        try (Table table = connection.getTable(TableName.valueOf("test"))) {
+            Scan scan = new Scan();
+            scan.setFilter(new FamilyFilter(CompareFilter.CompareOp.EQUAL,
+                    new BinaryComparator(config.getContentColumnFamily())));
+            try (ResultScanner scanner = table.getScanner(scan)){
+                for (Result result : scanner) {
+                    String link = Bytes.toString(result.getRow());
+                    String content = Bytes.toString(result.getValue(config.getContentColumnFamily(),
+                            config.getContentColumn()));
+                    Document document = Jsoup.parse(content);
+                    String pageContentWithoutTag = document.text().replace("\n", " ");
+                    Set<Anchor> anchors = parserService.getAnchors(document);
+                    List<Meta> metas = parserService.getMetas(document);
+                    String title = parserService.getTitle(document);
+                    Page page = new Page(link, title, "", pageContentWithoutTag, anchors, metas, 1.0);
+                    elasticDAO.save(page);
+                }
+            }
         } catch (IOException e) {
             throw new HBaseException(e);
         }
