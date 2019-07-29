b89d09285878135a9109c3a57f37725d9fb04399
MohammadReza Pakzadian <pakzadianmrp@gmail.com>
2019-07-29 17:33:26 +0430

    add equals method to Anchor class

--- crawler/src/main/java/in/nimbo/entity/Anchor.java
+++ crawler/src/main/java/in/nimbo/entity/Anchor.java
@@ -1,5 +1,7 @@
 package in.nimbo.entity;
 
+import java.util.Objects;
+
 public class Anchor {
     private String href;
     private String content;
@@ -15,5 +17,19 @@ public class Anchor {
 
     public String getContent() {
         return content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Anchor anchor = (Anchor) o;
+        return Objects.equals(href, anchor.href) &&
+                Objects.equals(content, anchor.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(href, content);
     }
 }
