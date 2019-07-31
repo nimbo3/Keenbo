1d4b1fac63c048b706b823175404a51f186c141c
ehsan karimi <karimiehsan901@gmail.com>
2019-07-31 11:19:24 +0430

    Show memory usage of each node for redis

--- crawler/src/main/java/in/nimbo/dao/redis/RedisDAO.java
+++ crawler/src/main/java/in/nimbo/dao/redis/RedisDAO.java
@@ -1,7 +1,13 @@
 package in.nimbo.dao.redis;
 
+import in.nimbo.entity.RedisNodeStatus;
+
+import java.util.List;
+
 public interface RedisDAO {
     void add(String link);
 
     boolean contains(String link);
+
+    List<RedisNodeStatus> memoryUsage();
 }
