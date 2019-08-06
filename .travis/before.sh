0a1e5d4334f8a599331a1a30882ac685d886ab0e
ehsan karimi <karimiehsan901@gmail.com>
2019-08-06 12:50:58 +0430

    Log script in travis

--- .travis/before.sh
+++ .travis/before.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 if [[ ! -f $HOME/downloads/hbase-1.2.4-bin.tar.gz ]];
 then
+    echo "Downloading"
     sudo wget -O $HOME/downloads/hbase-1.2.4-bin.tar.gz https://archive.apache.org/dist/hbase/1.2.4/hbase-1.2.4-bin.tar.gz
 fi
-sudo mv $HOME/downloads/hbase-1.2.4-bin.tar.gz hbase-1.2.4-bin.tar.gz
+echo "moving"
+sudo mv $HOME/downloads/hbase-1.2.4-bin.tar.gz hbase-1.2.4.tar.gz
+echo "Extracting"
 sudo tar -xvzf hbase-1.2.4-bin.tar.gz
+echo "Config ..."
 sudo mv .travis/hbase-site.xml hbase-1.2.4/conf
+echo "Running HBase"
 sudo hbase-1.2.4/bin/start-hbase.sh
+echo "HBase Runned"
