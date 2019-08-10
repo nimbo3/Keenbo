40718584a22d120c5fc395c16cf61068ae8336ea
MohammadReza Pakzadian <pakzadianmrp@gmail.com>
2019-08-10 17:10:32 +0430

    add timestamp field to Page class for adding to elastic

--- forward-extractor/src/main/java/in/nimbo/entity/Page.java
+++ forward-extractor/src/main/java/in/nimbo/entity/Page.java
@@ -1,11 +1,13 @@
 package in.nimbo.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Page implements Serializable {
     private String id;
     private Iterable<String> anchors;
     private long anchorsSize;
+    private long timestamp;
 
     public Page() {
     }
@@ -14,6 +16,7 @@ public class Page implements Serializable {
         this.id = id;
         this.anchors = anchors;
         this.anchorsSize = anchorsSize;
+        this.timestamp = new Date().getTime();
     }
 
     public String getId() {
@@ -38,5 +41,21 @@ public class Page implements Serializable {
 
     public void setLength(int anchorsLength) {
         this.anchorsSize = anchorsLength;
+    }
+
+    public long getAnchorsSize() {
+        return anchorsSize;
+    }
+
+    public void setAnchorsSize(long anchorsSize) {
+        this.anchorsSize = anchorsSize;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
     }
 }
