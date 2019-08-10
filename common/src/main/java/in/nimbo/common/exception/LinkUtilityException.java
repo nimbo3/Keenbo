4488f9e37cefb1f727891078ea9e0ceaf5b941b9
MohammadReza Pakzadian <pakzadianmrp@gmail.com>
2019-08-10 16:32:44 +0430

    change LinkUtility.reverseLink method to throw LinkUtilityException

---
+++ common/src/main/java/in/nimbo/common/exception/LinkUtilityException.java
@@ -0,0 +1,11 @@
+package in.nimbo.common.exception;
+
+public class LinkUtilityException extends RuntimeException {
+    public LinkUtilityException() {
+        super();
+    }
+
+    public LinkUtilityException(Throwable cause) {
+        super(cause);
+    }
+}
