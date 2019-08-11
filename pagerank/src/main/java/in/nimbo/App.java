e0e044690ec97ab3cf9044906936beabe8375f4d
ehsan karimi <karimiehsan901@gmail.com>
2019-08-11 19:01:25 +0430

    Fix all problems and fully save in HBase

--- pagerank/src/main/java/in/nimbo/App.java
+++ pagerank/src/main/java/in/nimbo/App.java
@@ -2,35 +2,26 @@ package in.nimbo;
 
 import in.nimbo.common.config.HBaseConfig;
 import in.nimbo.common.utility.LinkUtility;
-import in.nimbo.config.PageRankConfig;
 import in.nimbo.entity.Page;
 import in.nimbo.entity.Relation;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
-import org.apache.hadoop.hbase.mapred.TableOutputFormat;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.spark.SparkConf;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.graphx.Graph;
-import org.apache.spark.graphx.impl.GraphImpl;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.graphframes.GraphFrame;
 import scala.Tuple2;
-import scala.collection.immutable.Map;
 
 import java.io.IOException;
-import java.io.Serializable;
-import java.util.NavigableMap;
-import java.util.Set;
 
 public class App {
     public static void main(String[] args) {
@@ -42,9 +33,9 @@ public class App {
         hBaseConfiguration.addResource(System.getenv("HADOOP_HOME") + "/etc/hadoop/core-site.xml");
         hBaseConfiguration.addResource(System.getenv("HBASE_HOME") + "/conf/hbase-site.xml");
         hBaseConfiguration.set(TableInputFormat.INPUT_TABLE, hBaseConfig.getLinksTable());
-        hBaseConfiguration.set(TableInputFormat.SCAN_BATCHSIZE, "2000");
 
         SparkSession spark = SparkSession.builder()
+                .config("spark.hadoop.validateOutputSpecs", false)
                 .appName("pagerank")
                 .master("local")
                 .getOrCreate();
@@ -76,35 +67,24 @@ public class App {
         Dataset<Row> edgDF = spark.createDataFrame(edges, Relation.class);
 
         GraphFrame graphFrame = new GraphFrame(verDF, edgDF);
-        GraphFrame pageRank = graphFrame.pageRank().maxIter(20).resetProbability(0.01).run();
+        GraphFrame pageRank = graphFrame.pageRank().maxIter(1).resetProbability(0.01).run();
         pageRank.vertices().sort("pagerank").show(2000, false);
         JavaRDD<Row> pageRankRdd = pageRank.vertices().toJavaRDD();
-        JavaPairRDD<String, Put> javaPairRDD = pageRankRdd.mapToPair(row -> {
-            Put put = new Put(Bytes.toBytes(row.getString(1)));
-            put.addColumn(Bytes.toBytes("R"), Bytes.toBytes("R"), Bytes.toBytes(String.valueOf(row.getDouble(2))));
-            return new Tuple2<>(row.getString(1), put);
+        JavaPairRDD<ImmutableBytesWritable, Put> javaPairRDD = pageRankRdd.mapToPair(row -> {
+            Put put = new Put(Bytes.toBytes(row.getString(0)));
+            put.addColumn(Bytes.toBytes("R"), Bytes.toBytes("R"), Bytes.toBytes(String.valueOf(row.getDouble(1))));
+            return new Tuple2<>(new ImmutableBytesWritable(), put);
         });
-        JobConf jobConf = new JobConf(hBaseConfiguration);
-        jobConf.set(TableOutputFormat.OUTPUT_TABLE, "dummy_page");
-        javaPairRDD.saveAsNewAPIHadoopDataset(jobConf);
+        try {
+            Job jobConf = Job.getInstance();
+            jobConf.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, "dummy_page");
+            jobConf.setOutputFormatClass(TableOutputFormat.class);
+            jobConf.getConfiguration().set("mapreduce.output.fileoutputformat.outputdir", "/tmp");
+            javaPairRDD.saveAsNewAPIHadoopDataset(jobConf.getConfiguration());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         spark.stop();
-
-            /*JavaPairRDD<Set<byte[]>, Double> map = hBaseRDD
-                    .mapToPair(result -> {
-                        double rank = Bytes.toDouble(result.getValue(Bytes.toBytes("R"), Bytes.toBytes("R")));
-                        NavigableMap<byte[], byte[]> familyMap = result.getFamilyMap(Bytes.toBytes("A"));
-                        int count = familyMap.keySet().size();
-                        rank /= count;
-                        return new Tuple2<>(familyMap.keySet(), rank);
-                    });
-
-            JavaPairRDD<byte[], Double> flatMapToPair = map.flatMapToPair(setRank -> setRank._1.stream().map(bytes ->
-                    new Tuple2<>(bytes, setRank._2)).iterator());
-
-            JavaPairRDD<byte[], Double> reduced = flatMapToPair.reduceByKey((v1, v2) -> (v1 + v2));
-            reduced.saveAsTextFile("result.txt");*/
-
-
     }
 }
