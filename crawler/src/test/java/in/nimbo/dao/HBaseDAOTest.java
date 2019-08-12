8d7f50506f81b5fb1ea4a4c3e53a0998d3c9a378
Amin Borjian <borjianamin98@outlook.com>
2019-08-12 15:58:48 +0430

    Add more hbase tests

--- crawler/src/test/java/in/nimbo/dao/HBaseDAOTest.java
+++ crawler/src/test/java/in/nimbo/dao/HBaseDAOTest.java
@@ -22,6 +22,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class HBaseDAOTest {
     private static HBaseDAO hBaseDAO;
     private static HBaseConfig hBaseConfig;
@@ -59,12 +62,12 @@ public class HBaseDAOTest {
         }
         Page page = new Page("http://www.google.com/", "Google", "content", anchors, metas, 100.0);
         hBaseDAO.add(page);
-//        assertTrue(hBaseDAO.contains(page.getLink()));
+        assertTrue(hBaseDAO.contains(page.getLink()));
     }
 
-//    @Test
-//    public void testContain() {
-//        assertFalse(hBaseDAO.contains("http://www.google.com/"));
-//        assertFalse(hBaseDAO.contains("fake link"));
-//    }
+    @Test
+    public void testContain() {
+        assertFalse(hBaseDAO.contains("http://www.google.com/"));
+        assertFalse(hBaseDAO.contains("fake link"));
+    }
 }
