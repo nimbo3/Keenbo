34f918480937fdecf182be34268b544d13b33985
Amin Borjian <borjianamin98@outlook.com>
2019-08-12 11:39:00 +0430

    Fix Bash code

--- .travis/before.sh
+++ .travis/before.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 mkdir /home/travis/hbase /home/travis/zookeeper
-if [[ ! -f $HOME/Downloads/hbase-1.2.4-bin.tar.gz ]];
+if [[ ! -d $HOME/Downloads/hbase-1.2.4-bin ]];
 then
-    echo "Downloading"
+    echo "Downloading HBase"
     sudo wget -O $HOME/Downloads/hbase-1.2.4-bin.tar.gz https://archive.apache.org/dist/hbase/1.2.4/hbase-1.2.4-bin.tar.gz
+    echo "Extracting HBase Files"
+    tar -xvzf hbase-1.2.4.tar.gz
 fi
-echo "moving"
-mv $HOME/Downloads/hbase-1.2.4-bin.tar.gz hbase-1.2.4.tar.gz
-echo "Extracting"
-tar -xvzf hbase-1.2.4.tar.gz
+echo "Copying HBase Files"
+cp -r $HOME/Downloads/hbase-1.2.4-bin hbase-1.2.4
 echo "Config ..."
-mv .travis/hbase-site.xml hbase-1.2.4/conf
+cp .travis/hbase-site.xml hbase-1.2.4/conf/
 echo "Running HBase"
 #echo "export HBASE_HOME=/home/travis/build/nimbo3/Keenbo/hbase-1.2.4" >> /home/travis/.bashrc
 #source /home/travis/.bashrc
