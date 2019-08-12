67977b291c834966872e2afe1c6df70e9a399003
Amin Borjian <borjianamin98@outlook.com>
2019-08-12 12:02:06 +0430

    Fix Bash code

--- .travis/before.sh
+++ .travis/before.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 export HBASE_DOWNLOAD_FILE="$HOME/Downloads/hbase-1.2.4-bin.tar.gz"
+export HBASE_FILES="hbase-1.2.4"
 
 mkdir /home/travis/hbase /home/travis/zookeeper
 if [[ ! -d $HOME/Downloads/hbase-1.2.4-bin ]];
@@ -10,16 +11,16 @@ then
     tar -xvzf $HBASE_DOWNLOAD_FILE
 fi
 echo "Copying HBase Files"
-cp -r $HBASE_DOWNLOAD_FILE hbase-1.2.4
+cp -r $HBASE_DOWNLOAD_FILE $HBASE_FILES
 echo "Config ..."
-cp .travis/hbase-site.xml hbase-1.2.4/conf/
+cp .travis/hbase-site.xml $HBASE_FILES/conf/
 echo "Running HBase"
 #echo "export HBASE_HOME=/home/travis/build/nimbo3/Keenbo/hbase-1.2.4" >> /home/travis/.bashrc
 #source /home/travis/.bashrc
 #sudo chmod -R 777 hbase-1.2.4
-sudo hbase-1.2.4/bin/start-hbase.sh
+sudo $HBASE_FILES/bin/start-hbase.sh
 echo "Preparing HBase"
-#echo 'list' | hbase-1.2.4/bin/hbase shell -n
+echo 'list' | $HBASE_FILES/bin/hbase shell -n
 #echo 'create "page", "meta", "anchor"' | hbase-1.2.4/bin/hbase shell -n
 #echo 'list' | hbase-1.2.4/bin/hbase shell -n
 echo "HBase is ready"
