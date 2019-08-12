41495f97651f0bf35fc3e0f1271b6cd16beeee8e
Amin Borjian <borjianamin98@outlook.com>
2019-08-12 15:00:45 +0430

    Add .travis for scripts

---
+++ .travis/before.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+export HBASE_DOWNLOAD_FILE="$HOME/Downloads/hbase-1.2.4-bin.tar.gz"
+export HBASE_FILES="hbase-1.2.4"
+
+sudo mkdir -p $HOME/Downloads $HOME/hbase $HOME/zookeeper
+echo "Downloading HBase"
+sudo wget -O $HBASE_DOWNLOAD_FILE https://archive.apache.org/dist/hbase/1.2.4/hbase-1.2.4-bin.tar.gz
+
+echo "Copying HBase Files"
+cp $HBASE_DOWNLOAD_FILE hbase-1.2.4.tar.gz
+
+echo "Extracting HBase Files"
+tar -xvzf hbase-1.2.4.tar.gz > /dev/null 2>&1
+
+echo "HBase Configuration"
+cp .travis/hbase-site.xml $HBASE_FILES/conf/
+echo "export JAVA_HOME=$JAVA_HOME" >> $HBASE_FILES/conf/hbase-env.sh
+
+echo "Running HBase"
+sudo $HBASE_FILES/bin/start-hbase.sh
