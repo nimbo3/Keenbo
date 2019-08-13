b2860341c5454a153e4d4e7628a446eae4f793e4
MohammadReza Pakzadian <pakzadianmrp@gmail.com>
2019-08-13 15:47:52 +0430

    refactore map reduce on edges

--- site-graph/src/main/java/in/nimbo/App.java
+++ site-graph/src/main/java/in/nimbo/App.java
@@ -10,12 +10,14 @@ import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
 import org.graphframes.GraphFrame;
+import scala.Tuple2;
 
 public class App {
     public static void main(String[] args) {
@@ -50,55 +52,43 @@ public class App {
                     return new Node(getMainDomainForReversed(Bytes.toString(result.getRow())), rank);
                 });
 
-        JavaRDD<Edge> edges = hBaseRDD
-                .flatMap(result -> result.getFamilyMap(anchorColumnFamily).entrySet().stream().map(
+        JavaPairRDD<String, String> links = hBaseRDD
+                .flatMapToPair(result -> result.getFamilyMap(anchorColumnFamily).entrySet().stream().map(
                         entry -> {
                             String anchorLink = Bytes.toString(entry.getKey());
                             int index = anchorLink.indexOf("#");
                             if (index != -1)
                                 anchorLink = anchorLink.substring(0, index);
-                            anchorLink = getMainDomain(anchorLink);
-                            return new Edge(getMainDomainForReversed(Bytes.toString(result.getRow())), anchorLink);
+                            return new Tuple2<>(Bytes.toString(result.getRow()), anchorLink);
                         }).iterator());
+        JavaPairRDD<String, String> mainDomains = links.mapToPair(link ->
+                new Tuple2<>(getMainDomainForReversed(link._1), getMainDomain(link._2)));
 
-        Dataset<Row> verDF = spark.createDataFrame(nodes, Node.class);
+        JavaPairRDD<String, String> filteredMainDomains = mainDomains.filter(domain -> !domain._1.equals(domain._2));
 
-        Dataset<Row> edgDF = spark.createDataFrame(edges, Edge.class);
-
-        GraphFrame graphFrame = new GraphFrame(verDF, edgDF);
-        Dataset<Row> edgesWithWeight = graphFrame.triplets().groupBy("src", "dst")
-                .agg(functions.sum("edge.numOfAnchors"));
-
-        edgesWithWeight.show();
-//        JavaRDD<Page> anchorsRDD = anchors.toJavaRDD()
-//                .map(row -> new Page(
-//                        LinkUtility.hashLink(LinkUtility.reverseLink(row.getStruct(0).getString(0))),
-//                        row.getList(1), row.getLong(2)));
-
-//        JavaEsSpark.saveToEs(anchorsRDD, appConfig.getEsIndexName() + "/" + appConfig.getEsType());
+        JavaRDD<Edge> edges = filteredMainDomains.map(link ->
+                new Edge(link._1, link._2));
 
         spark.stop();
     }
 
     private static String getMainDomain(String link) {
-//        String linkWithoutProtocol = link.substring(link.indexOf('/') + 2);
-//        int indexOfSlash = linkWithoutProtocol.indexOf('/');
-//        if (indexOfSlash != -1) {
-//            linkWithoutProtocol = linkWithoutProtocol.substring(0, indexOfSlash);
-//        }
-        String linkWithoutProtocol = link;
+        String linkWithoutProtocol = link.substring(link.indexOf('/') + 2);
+        int indexOfSlash = linkWithoutProtocol.indexOf('/');
+        if (indexOfSlash != -1) {
+            linkWithoutProtocol = linkWithoutProtocol.substring(0, indexOfSlash);
+        }
         int lastDot = linkWithoutProtocol.lastIndexOf('.');
         int beforeLastDot = linkWithoutProtocol.substring(0, lastDot).lastIndexOf('.');
         return beforeLastDot == -1 ? linkWithoutProtocol : linkWithoutProtocol.substring(beforeLastDot + 1);
     }
 
     private static String getMainDomainForReversed(String link) {
-//        String linkWithoutProtocol = link.substring(link.indexOf('/') + 2);
-//        int indexOfSlash = linkWithoutProtocol.indexOf('/');
-//        if (indexOfSlash != -1) {
-//            linkWithoutProtocol = linkWithoutProtocol.substring(0, indexOfSlash);
-//        }
-        String linkWithoutProtocol = link;
+        String linkWithoutProtocol = link.substring(link.indexOf('/') + 2);
+        int indexOfSlash = linkWithoutProtocol.indexOf('/');
+        if (indexOfSlash != -1) {
+            linkWithoutProtocol = linkWithoutProtocol.substring(0, indexOfSlash);
+        }
         int firstDot = linkWithoutProtocol.indexOf('.');
         int afterFirstDot = linkWithoutProtocol.substring(firstDot + 1).indexOf('.');
         if (afterFirstDot != -1)
