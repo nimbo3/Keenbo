88d3243cf002576c36365dcd291404c5aee0626f
Amin Borjian <borjianamin98@outlook.com>
2019-08-17 22:45:41 +0430

    Fix page deserializer

--- common/src/main/java/in/nimbo/common/serializer/PageDeserializer.java
+++ common/src/main/java/in/nimbo/common/serializer/PageDeserializer.java
@@ -1,9 +1,7 @@
 package in.nimbo.common.serializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
 import in.nimbo.common.entity.Page;
-import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.header.Headers;
 import org.apache.kafka.common.serialization.Deserializer;
@@ -11,9 +9,7 @@ import org.apache.kafka.common.serialization.Deserializer;
 import java.io.IOException;
 import java.util.Map;
 
-;
-
-public class PageDeserializer implements Deserializer<Page> { // TODO
+public class PageDeserializer implements Deserializer<Page> {
     @Override
     public void configure(Map<String, ?> configs, boolean isKey) {
 
@@ -28,7 +24,7 @@ public class PageDeserializer implements Deserializer<Page> { // TODO
     public Page deserialize(String topic, Headers headers, byte[] data) {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            return mapper.readValue(Bytes.toString(data), Page.class);
+            return mapper.readValue(data, Page.class);
         } catch (IOException e) {
             throw new SerializationException(e);
         }
