44db215cc4b2eddb71d1a656232290ae923ac2fc
Amin Borjian <borjianamin98@outlook.com>
2019-08-25 14:01:34 +0430

    Fix elasticsearch tests

--- page-collector/src/test/java/in/nimbo/dao/ElasticDAOTest.java
+++ page-collector/src/test/java/in/nimbo/dao/ElasticDAOTest.java
@@ -2,24 +2,20 @@ package in.nimbo.dao;
 
 import in.nimbo.App;
 import in.nimbo.common.config.ElasticConfig;
+import in.nimbo.common.entity.Meta;
 import in.nimbo.common.entity.Page;
 import in.nimbo.dao.elastic.ElasticBulkListener;
 import in.nimbo.dao.elastic.ElasticDAO;
 import in.nimbo.dao.elastic.ElasticDAOImpl;
-import in.nimbo.common.entity.Meta;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.action.bulk.BulkProcessor;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.common.settings.Settings;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import pl.allegro.tech.embeddedelasticsearch.EmbeddedElastic;
-import pl.allegro.tech.embeddedelasticsearch.PopularProperties;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -30,46 +26,32 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ElasticDAOTest {
-    private static ElasticConfig elasticConfig;
     private static ElasticDAO elasticDAO;
     private static RestHighLevelClient client;
     private static List<Page> backupPages;
-    private static BulkProcessor bulkProcessor;
-    private static EmbeddedElastic embeddedElastic;
 
     @BeforeClass
-    public static void init() throws IOException, InterruptedException {
-        elasticConfig = ElasticConfig.load();
+    public static void init() throws IOException {
+        ElasticConfig elasticConfig = ElasticConfig.load();
         elasticConfig.setHost("localhost");
         elasticConfig.setBulkActions(2);
         elasticConfig.setBulkSize(1);
         elasticConfig.setIndexName("test-index");
 
-        CreateIndexRequest request = new CreateIndexRequest(elasticConfig.getIndexName());
-        CreateIndexResponse createIndexResponse = client.indices().create(request, RequestOptions.DEFAULT);
-
-//        embeddedElastic = EmbeddedElastic.builder()
-//                .withElasticVersion("6.6.2")
-//                .withDownloadDirectory(new File(System.getenv("$HOME") + "/Downloads"))
-//                .withSetting(PopularProperties.TRANSPORT_TCP_PORT, 9350)
-//                .withSetting(PopularProperties.CLUSTER_NAME, "cluster")
-//                .withEsJavaOpts("-Xms128m -Xmx512m")
-//                .withIndex(elasticConfig.getIndexName())
-//                .withStartTimeout(3, TimeUnit.MINUTES)
-//                .build()
-//                .start();
-
         backupPages = new ArrayList<>();
         client = App.initializeElasticSearchClient(elasticConfig);
         ElasticBulkListener elasticBulkListener = new ElasticBulkListener(backupPages);
-        bulkProcessor = App.initializeElasticSearchBulk(elasticConfig, client, elasticBulkListener);
+        BulkProcessor bulkProcessor = App.initializeElasticSearchBulk(elasticConfig, client, elasticBulkListener);
         elasticDAO = new ElasticDAOImpl(elasticConfig, bulkProcessor, backupPages, client);
         elasticBulkListener.setElasticDAO(elasticDAO);
+
+        CreateIndexRequest request = new CreateIndexRequest(elasticConfig.getIndexName());
+        client.indices().create(request, RequestOptions.DEFAULT);
     }
 
     @AfterClass
-    public static void stop() {
-        embeddedElastic.stop();
+    public static void stop() throws IOException {
+        client.close();
     }
 
     @Test
