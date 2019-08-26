c9f259e22332a62068b823973c79d3757a0f35a3
Amin Borjian <borjianamin98@outlook.com>
2019-08-26 23:41:20 +0430

    Fix shuffler to use hash link

--- shuffler/src/main/java/in/nimbo/service/ShufflerService.java
+++ shuffler/src/main/java/in/nimbo/service/ShufflerService.java
@@ -5,6 +5,7 @@ import com.codahale.metrics.SharedMetricRegistries;
 import com.codahale.metrics.Timer;
 import in.nimbo.common.config.KafkaConfig;
 import in.nimbo.common.utility.CloseUtility;
+import in.nimbo.common.utility.LinkUtility;
 import in.nimbo.config.ShufflerConfig;
 import in.nimbo.redis.RedisDAO;
 import org.apache.kafka.clients.consumer.CommitFailedException;
@@ -25,6 +26,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 public class ShufflerService implements Runnable, Closeable {
     private Logger logger = LoggerFactory.getLogger("shuffler");
@@ -123,7 +125,8 @@ public class ShufflerService implements Runnable, Closeable {
 
     private void processList() {
         logger.info("Start filtering {} links", shuffleList.size());
-        List<?> containStatus = redisDAO.contains(shuffleList);
+        List<String> hashedList = shuffleList.stream().map(LinkUtility::hashLinkCompressed).collect(Collectors.toList());
+        List<?> containStatus = redisDAO.contains(hashedList);
         List<String> filteredLinks = new ArrayList<>();
         for (int i = 0; i < containStatus.size(); i++) {
             Boolean contain = (Boolean) containStatus.get(i);
