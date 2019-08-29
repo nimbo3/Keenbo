8ab12f61f73c35dea02779d7785110bc519806d7
Amin Borjian <borjianamin98@outlook.com>
2019-08-29 15:15:49 +0430

    fix tests

--- page-collector/src/test/java/in/nimbo/service/keyword/KeywordExtractorServiceTest.java
+++ page-collector/src/test/java/in/nimbo/service/keyword/KeywordExtractorServiceTest.java
@@ -1,8 +1,6 @@
 package in.nimbo.service.keyword;
 
-import in.nimbo.service.keyword.KeywordExtractorService;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
