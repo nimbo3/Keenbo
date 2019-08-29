f5f3fe446e7c57f0d7f8743231606101524a2163
ehsan karimi <karimiehsan901@gmail.com>
2019-08-29 13:27:56 +0430

    Create database and tables

---
+++ search/src/main/resources/init.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS keenbo;
+CREATE DATABASE keenbo;
+USE keenbo;
+
+CREATE TABLE users (
+  username VARCHAR(100) PRIMARY KEY,
+  password TEXT NOT NULL ,
+  name TEXT NOT NULL ,
+  email TEXT NOT NULL ,
+  token TEXT NOT NULL
+);
+
+CREATE TABLE clicks (
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(100) NOT NULL,
+  destination VARCHAR(2048) NOT NULL,
+  FOREIGN KEY (username) REFERENCES users(username)
+);
