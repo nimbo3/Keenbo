7ba6261aa93c6d75a27e056cb16cefe0c8339986
Amin Borjian <borjianamin98@outlook.com>
2019-08-29 13:46:07 +0430

    Use better library for keyword extracting
    Former-commit-id: 7c0ea5ed6d57ef0f74d35169c9942eb42ae112a3

--- page-collector/src/test/java/in/nimbo/service/KeywordExtractorServiceTest.java
+++ page-collector/src/test/java/in/nimbo/service/KeywordExtractorServiceTest.java
@@ -2,47 +2,57 @@ package in.nimbo.service;
 
 import in.nimbo.service.keyword.KeywordExtractorService;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class KeywordExtractorServiceTest {
+    private static KeywordExtractorService keywordExtractorService;
+
+    @BeforeClass
+    public static void init() throws IOException {
+        keywordExtractorService = new KeywordExtractorService();
+    }
+
     @Test
     public void extractKeywordTest() {
         String text = "nimbo sahab. nimbo nimbo sahab nimbo.";
         Map<String, Integer> keywords = new HashMap<>();
         keywords.put("nimbo", 4);
         keywords.put("sahab", 2);
-        Assert.assertEquals(keywords, KeywordExtractorService.extractKeywords(text));
+        Assert.assertEquals(keywords, keywordExtractorService.extractKeywords(text));
     }
 
     @Test
     public void stopWordsTest() {
         String text = "he him could that were was.";
         Map<String, Integer> keywords = new HashMap<>();
-        Assert.assertEquals(keywords, KeywordExtractorService.extractKeywords(text));
+        Assert.assertEquals(keywords, keywordExtractorService.extractKeywords(text));
     }
 
     @Test
     public void moreThanFiveWordsTest() {
-        String text = "nimbo sahab nimbo keenbo datapirates sahab nimbo jimbo keenbo nimbo datapirates keenbo nimroo jimbo";
+        String text = "nimbo sahab nimbo keenbo datapirate sahab nimbo jimbo keenbo nimbo datapirate keenbo nimroo jimbo";
         Map<String, Integer> keywords = new HashMap<>();
         keywords.put("nimbo", 4);
         keywords.put("keenbo", 3);
         keywords.put("sahab", 2);
         keywords.put("datapirate", 2);
         keywords.put("jimbo", 2);
-        Assert.assertEquals(keywords, KeywordExtractorService.extractKeywords(text));
+        Assert.assertEquals(keywords, keywordExtractorService.extractKeywords(text));
     }
 
     @Test
     public void wordRootTest() {
-        String text = "driving drove foots came";
+        String text = "driving drove foot came";
         Map<String, Integer> keywords = new HashMap<>();
         keywords.put("drive", 2);
         keywords.put("come", 1);
         keywords.put("foot", 1);
-        Assert.assertEquals(keywords, KeywordExtractorService.extractKeywords(text));
+        Assert.assertEquals(keywords, keywordExtractorService.extractKeywords(text));
     }
 }
