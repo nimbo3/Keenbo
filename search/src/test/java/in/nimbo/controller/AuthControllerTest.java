596b609daa3186f44921b4523cc4de209d3cabe9
ehsan karimi <karimiehsan901@gmail.com>
2019-08-29 12:09:51 +0430

    Test controller layer

---
+++ search/src/test/java/in/nimbo/controller/AuthControllerTest.java
@@ -0,0 +1,111 @@
+package in.nimbo.controller;
+
+import in.nimbo.config.SparkConfig;
+import in.nimbo.dao.auth.AuthDAO;
+import in.nimbo.entity.User;
+import in.nimbo.response.ActionResult;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class AuthControllerTest {
+    private static AuthController controller;
+    private static AuthDAO dao;
+    private static SparkConfig config;
+
+    @BeforeClass
+    public static void init() {
+        config = SparkConfig.load();
+        dao = mock(AuthDAO.class);
+        controller = new AuthController(dao, config);
+    }
+
+    @Test
+    public void testTrueRegister() {
+        String username = "admin";
+        String password = "password";
+        String confirmPass = "password";
+        String email = "email@gmail.com";
+        String token = "token";
+        String name = "name";
+        User user = new User(name, email, username, email, token);
+        when(dao.register(username, password, email, name)).thenReturn(user);
+        when(dao.containsUserName(username)).thenReturn(false);
+        ActionResult<User> register = controller.register(username, password, confirmPass, email, name);
+        assertTrue(register.isSuccess());
+        assertEquals(register.getData(), user);
+    }
+
+    @Test
+    public void testFalseRegister() {
+        String username = " admin ";
+        String password = "aa";
+        String confirmPass = "a";
+        String email = "admin";
+        String name = "";
+        ActionResult<User> register = controller.register(username, password, confirmPass, email, name);
+        assertTrue(register.getMessage().contains(config.getEmailInvalidError()));
+        assertTrue(register.getMessage().contains(config.getNameError()));
+        assertTrue(register.getMessage().contains(config.getPasswordWeakError()));
+        assertTrue(register.getMessage().contains(config.getEmailInvalidError()));
+        assertTrue(register.getMessage().contains(config.getNameError()));
+        assertFalse(register.isSuccess());
+        assertNull(register.getData());
+        password = "aaaaaa";
+        register = controller.register(username, password, confirmPass, email, name);
+        assertTrue(register.getMessage().contains(config.getPasswordUnlikeError()));
+        assertFalse(register.isSuccess());
+        assertNull(register.getData());
+        register = controller.register(null, null, null, null, null);
+        assertTrue(register.getMessage().contains(config.getEmailInvalidError()));
+        assertTrue(register.getMessage().contains(config.getNameError()));
+        assertTrue(register.getMessage().contains(config.getPasswordWeakError()));
+        assertTrue(register.getMessage().contains(config.getEmailInvalidError()));
+        assertTrue(register.getMessage().contains(config.getNameError()));
+        assertFalse(register.isSuccess());
+        assertNull(register.getData());
+    }
+
+    @Test
+    public void testDuplicateRegister() {
+        String username = "admin";
+        String password = "password";
+        String confirmPass = "password";
+        String email = "email@gmail.com";
+        String name = "name";
+        when(dao.containsUserName(username)).thenReturn(true);
+        ActionResult<User> register = controller.register(username, password, confirmPass, email, name);
+        assertFalse(register.isSuccess());
+        assertNull(register.getData());
+    }
+
+    @Test
+    public void testLogin() {
+        String username = "admin";
+        String password = "password";
+        User user = new User("name", "email", username, password, "token");
+        when(dao.authenticate(username, password)).thenReturn(user);
+        ActionResult<User> login = controller.login(username, password);
+        assertTrue(login.isSuccess());
+        assertEquals(login.getData(), user);
+        when(dao.authenticate(username, password)).thenReturn(null);
+        login = controller.login(username, password);
+        assertFalse(login.isSuccess());
+        assertNull(login.getData());
+        assertTrue(login.getMessage().equals(config.getLoginError()));
+    }
+
+    @Test
+    public void testClick() {
+        User user = new User();
+        String destination = "https://stackoverflow.com";
+        doNothing().when(dao).saveClick(user, destination);
+        ActionResult<Boolean> click = controller.click(user, destination);
+        assertTrue(click.isSuccess());
+        click = controller.click(null, destination);
+        assertFalse(click.isSuccess());
+
+    }
+}
