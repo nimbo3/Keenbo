76953b353b9423dac7e01b4f600f941f2b7b76b1
Amin Borjian <borjianamin98@outlook.com>
2019-08-30 10:04:43 +0430

    Fix FileUtility

--- common/src/main/java/in/nimbo/common/utility/FileUtility.java
+++ common/src/main/java/in/nimbo/common/utility/FileUtility.java
@@ -2,22 +2,19 @@ package in.nimbo.common.utility;
 
 import in.nimbo.common.exception.LoadResourceException;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStream;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class FileUtility {
     private FileUtility() {}
 
     public static String readFileFromResource(String resource) {
         try {
-            URL url = Thread.currentThread().getContextClassLoader().getResource(resource);
-            return new String(Files.readAllBytes(Paths.get(Objects.requireNonNull(url).toURI())), StandardCharsets.UTF_8);
-        } catch (IOException | URISyntaxException e) {
+            InputStream resourceAsStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(resource);
+            Scanner s = new Scanner(Objects.requireNonNull(resourceAsStream)).useDelimiter("\\A");
+            return s.hasNext() ? s.next() : "";
+        } catch (NullPointerException e) {
             throw new LoadResourceException(resource, e);
         }
     }
