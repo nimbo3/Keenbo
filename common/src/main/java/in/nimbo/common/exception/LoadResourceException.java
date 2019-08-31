8fe514bdbf955155dcf103902d87cde226db4663
ehsan karimi <karimiehsan901@gmail.com>
2019-08-31 15:03:20 +0430

    Delete remove load resource exception

--- common/src/main/java/in/nimbo/common/exception/LoadResourceException.java
+++
@@ -1,7 +0,0 @@
-package in.nimbo.common.exception;
-
-public class LoadResourceException extends RuntimeException {
-    public LoadResourceException(String resourceFile, Throwable cause) {
-        super("Unable to load file: " + resourceFile, cause);
-    }
-}
