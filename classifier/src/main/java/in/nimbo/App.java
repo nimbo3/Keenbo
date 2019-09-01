3b1695a74796076fa8ee3f1ee8a5a77e616c7df4
Amin Borjian <borjianamin98@outlook.com>
2019-09-01 18:41:14 +0430

    Fix classifier

--- classifier/src/main/java/in/nimbo/App.java
+++ classifier/src/main/java/in/nimbo/App.java
@@ -1,41 +1,16 @@
 package in.nimbo;
 
-import com.cybozu.labs.langdetect.LangDetectException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import in.nimbo.common.config.ElasticConfig;
-import in.nimbo.common.config.KafkaConfig;
 import in.nimbo.common.config.ProjectConfig;
-import in.nimbo.common.dao.elastic.ElasticDAO;
-import in.nimbo.common.dao.elastic.ElasticDAOImpl;
-import in.nimbo.common.entity.Link;
-import in.nimbo.common.service.ParserService;
-import in.nimbo.common.utility.LanguageDetectorUtility;
 import in.nimbo.common.utility.SparkUtility;
 import in.nimbo.config.ClassifierConfig;
-import in.nimbo.entity.Category;
 import in.nimbo.service.ClassifierService;
-import in.nimbo.service.CrawlerService;
-import in.nimbo.service.kafka.KafkaServiceImpl;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.sql.SparkSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.TimeUnit;
 
 public class App {
-    private static Logger logger = LoggerFactory.getLogger("classifier");
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         ProjectConfig projectConfig = ProjectConfig.load();
         ClassifierConfig classifierConfig = ClassifierConfig.load();
         if (classifierConfig.getAppMode() == ClassifierConfig.MODE.CRAWL) {
@@ -45,31 +20,8 @@ public class App {
         }
     }
 
-    private static void runCrawler(ClassifierConfig classifierConfig, ProjectConfig projectConfig) throws IOException {
-        LanguageDetectorUtility.loadLanguageDetector(logger);
+    private static void runCrawler(ClassifierConfig classifierConfig, ProjectConfig projectConfig) {
 
-        Cache<String, LocalDateTime> politenessCache = Caffeine.newBuilder().maximumSize(projectConfig.getCaffeineMaxSize())
-                .expireAfterWrite(projectConfig.getCaffeineExpireTime(), TimeUnit.SECONDS).build();
-        Cache<String, LocalDateTime> crawlerCache = Caffeine.newBuilder().build();
-
-        ElasticConfig elasticConfig = ElasticConfig.load();
-        KafkaConfig kafkaConfig = KafkaConfig.load();
-
-        ElasticDAO elasticDAO = ElasticDAOImpl.createElasticDAO(elasticConfig, new CopyOnWriteArrayList<>());
-
-        ObjectMapper mapper = new ObjectMapper();
-        List<Category> categories = CrawlerService.loadFeed(mapper);
-        Map<String, Double> labelMap = CrawlerService.loadLabels(categories);
-        List<String> domains = CrawlerService.loadDomains(categories);
-        BlockingQueue<Link> queue = new ArrayBlockingQueue<>(classifierConfig.getCrawlerQueueSize());
-        CrawlerService.fillInitialCrawlQueue(queue, categories);
-
-        ParserService parserService = new ParserService(projectConfig);
-        CrawlerService crawlerService = new CrawlerService(politenessCache, crawlerCache, parserService, elasticDAO, labelMap);
-        KafkaServiceImpl kafkaService = new KafkaServiceImpl(kafkaConfig, classifierConfig, crawlerService);
-        kafkaService.schedule();
-
-        Runtime.getRuntime().addShutdownHook(new Thread(kafkaService::stopSchedule));
     }
 
     private static void runClassifier(ClassifierConfig classifierConfig) {
--- classifier/src/main/java/in/nimbo/service/ClassifierService.java
+++ classifier/src/main/java/in/nimbo/service/ClassifierService.java
@@ -16,6 +16,7 @@ import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ClassifierService {
@@ -24,9 +25,19 @@ public class ClassifierService {
 
     public static void extractModel(ClassifierConfig classifierConfig, SparkSession spark,
                              JavaPairRDD<String, Map<String, Object>> elasticSearchRDD) {
+        Map<String, Double> labelMap = new HashMap<>();
+        labelMap.put("art", 0.0);
+        labelMap.put("science", 1.0);
+        labelMap.put("health", 2.0);
+        labelMap.put("news", 3.0);
+        labelMap.put("shopping", 4.0);
+        labelMap.put("sports", 5.0);
+        labelMap.put("social", 6.0);
+        labelMap.put("other", 7.0);
         JavaRDD<Data> dataRDD = elasticSearchRDD.map(tuple2 ->
-                new Data((Long) tuple2._2.get("label"), (String) tuple2._2.get("content")));
+                new Data(labelMap.get((String) tuple2._2.get("labelContent")), (String) tuple2._2.get("content")));
         Dataset<Row> dataset = spark.createDataFrame(dataRDD, Data.class);
+        dataset.show(false);
 
         Tokenizer tokenizer = new Tokenizer().setInputCol("content").setOutputCol("words");
         Dataset<Row> wordsData = tokenizer.transform(dataset);
@@ -40,7 +51,7 @@ public class ClassifierService {
         IDF idf = new IDF().setInputCol("rawFeatures").setOutputCol("feature");
         IDFModel idfModel = idf.fit(featuredData);
         try {
-            idf.save(classifierConfig.getNaiveBayesIDFSaveLocation());
+            idfModel.save(classifierConfig.getNaiveBayesIDFSaveLocation());
         } catch (IOException e) {
             System.out.println("Unable to save idf model: " + e.getMessage());
         }
@@ -54,15 +65,14 @@ public class ClassifierService {
                 .setFeaturesCol("feature");
 
         Dataset<Row>[] tmp = features.randomSplit(new double[]{0.8, 0.2});
-        Dataset<Row> training = tmp[0];
-        Dataset<Row> test = tmp[1];
+        Dataset<Row> training = features;
+        Dataset<Row> test = features;
 
         NaiveBayesModel model = naiveBayes.train(training);
         model.set("modelType", classifierConfig.getNaiveBayesModelType());
 
         JavaPairRDD<Double, Double> predictionAndLabel =
                 test.toJavaRDD().mapToPair((Row p) -> new Tuple2<>(model.predict(p.getAs(1)), p.getDouble(0)));
-        System.out.println(predictionAndLabel.collect());
 
         double accuracy = predictionAndLabel.filter(pl -> pl._1().equals(pl._2())).count() / (double) test.count();
         System.out.println(accuracy);
