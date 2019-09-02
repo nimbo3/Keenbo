6fdbd361f4185ff896ccfb04858e5ea81fa5b5bf
ehsan karimi <karimiehsan901@gmail.com>
2019-09-02 12:18:04 +0430

    Test site graph with parameter

--- search/src/test/java/in/nimbo/controller/GraphControllerTest.java
+++ search/src/test/java/in/nimbo/controller/GraphControllerTest.java
@@ -3,15 +3,24 @@ package in.nimbo.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import in.nimbo.common.config.HBaseConfig;
 import in.nimbo.common.dao.hbase.HBaseDAO;
+import in.nimbo.common.dao.hbase.HBaseDAOImpl;
 import in.nimbo.config.SparkConfig;
 import in.nimbo.entity.GraphResponse;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class GraphControllerTest {
     private static GraphController controller;
@@ -19,12 +28,39 @@ public class GraphControllerTest {
     private static SparkConfig sparkConfig;
 
     @BeforeClass
-    public static void init() {
-        hBaseDAO = mock(HBaseDAO.class);
+    public static void init() throws IOException {
+        Connection connection = ConnectionFactory.createConnection();
         HBaseConfig hBaseConfig = HBaseConfig.load();
+        hBaseDAO = new HBaseDAOImpl(connection, hBaseConfig);
         sparkConfig = SparkConfig.load();
         ObjectMapper mapper = new ObjectMapper();
         controller = new GraphController(hBaseDAO, hBaseConfig, sparkConfig, mapper);
+
+        Admin admin = connection.getAdmin();
+        HColumnDescriptor[] columnDescriptors = new HColumnDescriptor[2];
+        columnDescriptors[0] = new HColumnDescriptor(hBaseConfig.getDomainColumnFamily());
+        columnDescriptors[1] = new HColumnDescriptor(hBaseConfig.getInfoColumnFamily());
+        HTableDescriptor descriptor = new HTableDescriptor(TableName.valueOf(hBaseConfig.getSiteTable()));
+        descriptor.addFamily(columnDescriptors[0]).addFamily(columnDescriptors[1]);
+        admin.createTable(descriptor);
+
+        Table table = connection.getTable(TableName.valueOf(hBaseConfig.getSiteTable()));
+        List<Put> puts = new ArrayList<>();
+        Put stackoverflowPut = new Put(Bytes.toBytes("stackoverflow.com"));
+        stackoverflowPut.addColumn(hBaseConfig.getInfoColumnFamily(), hBaseConfig.getSiteRankColumn(), Bytes.toBytes("2"));
+        stackoverflowPut.addColumn(hBaseConfig.getInfoColumnFamily(), hBaseConfig.getCountColumn(), Bytes.toBytes("1"));
+        stackoverflowPut.addColumn(hBaseConfig.getDomainColumnFamily(), Bytes.toBytes("google.com"), Bytes.toBytes("4"));
+        stackoverflowPut.addColumn(hBaseConfig.getDomainColumnFamily(), Bytes.toBytes("facebook.com"), Bytes.toBytes("4"));
+        puts.add(stackoverflowPut);
+        Put googlePut = new Put(Bytes.toBytes("google.com"));
+        googlePut.addColumn(hBaseConfig.getInfoColumnFamily(), hBaseConfig.getSiteRankColumn(), Bytes.toBytes("8"));
+        googlePut.addColumn(hBaseConfig.getInfoColumnFamily(), hBaseConfig.getCountColumn(), Bytes.toBytes("1"));
+        puts.add(googlePut);
+        Put facebookPut = new Put(Bytes.toBytes("facebook.com"));
+        facebookPut.addColumn(hBaseConfig.getInfoColumnFamily(), hBaseConfig.getSiteRankColumn(), Bytes.toBytes("4"));
+        facebookPut.addColumn(hBaseConfig.getInfoColumnFamily(), hBaseConfig.getCountColumn(), Bytes.toBytes("1"));
+        puts.add(facebookPut);
+        table.put(puts);
     }
 
     @Test
@@ -61,5 +97,14 @@ public class GraphControllerTest {
         assertTrue(siteGraph.getEdges().get(1).getTo().equals("stackoverflow.com"));
         assertTrue(siteGraph.getEdges().get(0).getWidth() == sparkConfig.getMaxEdge());
         assertTrue(siteGraph.getEdges().get(1).getWidth() == sparkConfig.getMinEdge());
+    }
+
+    @Test
+    public void testSiteGraphWithParameter() throws IOException {
+        GraphResponse siteGraph = controller.siteGraph("stackoverflow.com");
+        assertTrue(siteGraph.getNodes().size() == 3);
+        assertTrue(siteGraph.getNodes().get(0).getId().equals("stackoverflow.com"));
+        assertTrue(siteGraph.getNodes().get(1).getId().equals("facebook.com"));
+        assertTrue(siteGraph.getNodes().get(2).getId().equals("google.com"));
     }
 }
